d60d69696d04b720885cc31a9de9232b2bdca17f
louisqvo <louisqvo@gmail.com>
2018-08-27 15:53:14 -0400

    small folders and files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Angus Burger", false);
